5feb27262b909dc66d11f3f2ef657eeff402d7f6
essamghonim <ee14eno@my.brunel.ac.uk>
2016-08-30 20:23:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ var api = new ParseServer({
   }
  push: {
 	ios: {
-	/*	pfx: 'cert.p12', // the path and filename to the .p12 file you exported earlier. 
+		pfx: 'cert.p12', // the path and filename to the .p12 file you exported earlier. 
 		passphrase: '',			
-		cert: 'cert.pem', // If not using the .p12 format, the path to the certificate PEM to load from disk
+	//	cert: 'cert.pem', // If not using the .p12 format, the path to the certificate PEM to load from disk
 		bundleId: 'Com.ConnectLtd.Pal', // The bundle identifier associated with your app
-		key: 'key.pem', // If not using the .p12 format, the path to the private key PEM to load from disk
+	//	key: 'key.pem', // If not using the .p12 format, the path to the private key PEM to load from disk
 		production: false // Specifies which environment to connect to: Production (if true) or Sandbox
-	*/	}
+		}
 	}
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
