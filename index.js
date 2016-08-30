25f7575329009665f69705b323ac4e2015fb70dd
essamghonim <ee14eno@my.brunel.ac.uk>
2016-08-30 20:23:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var api = new ParseServer({
   }
  push: {
 	ios: {
-		pfx: 'cert.p12', // the path and filename to the .p12 file you exported earlier. 
+		pfx: '/cert.p12', // the path and filename to the .p12 file you exported earlier. 
 		passphrase: '',			
 	//	cert: 'cert.pem', // If not using the .p12 format, the path to the certificate PEM to load from disk
 		bundleId: 'Com.ConnectLtd.Pal', // The bundle identifier associated with your app
