a453f21d43ace4bef677583069fdcabeef7069fc
essamghonim <ee14eno@my.brunel.ac.uk>
2016-08-31 22:36:17 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi Complete change Essam');
 });
 Parse.Cloud.define("iosPushTest", function(request, response) {
-Parse.initialize("essamghonimaucbrunel03121993", "myMasterKeyqazwsxedcrfvtgbyhnujm!@#$%^&*");
+//Parse.initialize("essamghonimaucbrunel03121993", "myMasterKeyqazwsxedcrfvtgbyhnujm!@#$%^&*");
 var pushQuery = new Parse.Query(Parse.Installation);
 pushQuery.equalTo('deviceType', 'ios');
 Parse.Push.send({
