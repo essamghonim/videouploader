7556e6283d6369742edcd18c6462809cd81fb4fc
essamghonim <ee14eno@my.brunel.ac.uk>
2016-08-31 22:20:13 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi Complete change Essam');
 });
 Parse.Cloud.define("iosPushTest", function(request, response) {
-Parse.initialize("essamghonimaucbrunel03121993", "myMasterKeyqazwsxedcrfvtgbyhnujm!@#$%^&*");
+//Parse.initialize("essamghonimaucbrunel03121993", "myMasterKeyqazwsxedcrfvtgbyhnujm!@#$%^&*");
 var pushQuery = new Parse.Query(Parse.Installation);
 pushQuery.equalTo('deviceToken', 'cd55c9c159d76100c4c12257cd639aa344ad9b025080f67b16aef57b4811c2af');
 Parse.Push.send({
