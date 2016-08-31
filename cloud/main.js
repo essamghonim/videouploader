49198d2b9db53032cf3df616c4a22fa44ae299f0
essamghonim <ee14eno@my.brunel.ac.uk>
2016-08-31 04:30:05 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -45,3 +45,29 @@ Parse.push.send({
 });
 
 });
+Parse.Cloud.afterSave("SendPush", function(request) {
+
+
+  var query = new Parse.Query(Parse.Installation);
+  query.exists("deviceToken");
+
+  // here you can add other conditions e.g. to send a push to sepcific users or channel etc.
+
+  var payload = {
+    alert: "YOUR_MESSAGE"
+      // you can add other stuff here...
+  };
+
+
+  Parse.Push.send({
+      data: payload,
+      where: query
+    }, {
+      useMasterKey: true
+    })
+    .then(function() {
+      response.success("Push Sent!");
+    }, function(error) {
+      response.error("Error while trying to send push " + error.message);
+    });
+});
