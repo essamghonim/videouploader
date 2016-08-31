b3abf68dbfb42ecbbfc9c3e68c3be01a62371351
essamghonim <ee14eno@my.brunel.ac.uk>
2016-08-31 17:59:32 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,45 +2,22 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi Complete change Essam');
 });
-Parse.Cloud.define("iosPushTest", function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
-  var params = request.params;
-  var user = request.user;
-
-  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
-  var messageText = params.text;
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
-
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
-    data: {
-      alert: "Message: " + messageText
-    }
-  }, { success: function() {
-      console.log("#### PUSH OK");
-  response.success('success');
-  }, error: function(error) {
-      console.log("#### PUSH ERROR" + error.message);
-  response.success('Didnot succeed');
-  }, useMasterKey: true});
-
-});
-Parse.Cloud.define("PushTest", function(request, response) {
+Parse.Cloud.afterSave("SendPush", function(request) {
 var query = new Parse.Query(Parse.Installation);
-Parse.Push.send({ 
-  where: query,
-  data: {
-    alert: "When do we take to outer space"
-  }
-}, {
-  success: function() {
-    console.log("Push was successful");
-  },
-  error: function(error) {
-    console.error(error);
-  }
-});
-
+  query.exists("deviceToken");
+  var payload = {
+    alert: "YOUR_MESSAGE"
+      // you can add other stuff here...
+  };
+  Parse.Push.send({
+      data: payload,
+      where: query
+    }, {
+      useMasterKey: true
+    })
+    .then(function() {
+      response.success("Push Sent!");
+    }, function(error) {
+      response.error("Error while trying to send push " + error.message);
+    });
 });
