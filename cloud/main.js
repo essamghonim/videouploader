5e8ad3a2ce7d23c9069ce7270e28da7c43860fbf
essamghonim <ee14eno@my.brunel.ac.uk>
2016-08-31 18:05:13 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -21,3 +21,24 @@ var query = new Parse.Query(Parse.Installation);
       response.error("Error while trying to send push " + error.message);
     });
 });
+Parse.Cloud.define('Hey', function(req, res) {
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo('deviceToken', 'cd55c9c159d76100c4c12257cd639aa344ad9b025080f67b16aef57b4811c2af');
+
+    Parse.Push.send({
+      where: query, // Set our Installation query
+      data: {
+        alert: "Willie Hayes injured by own pop fly."
+      }
+    },{
+      success: function() {
+        // Push was successful
+          res.success('HiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHi');
+      },
+      error: function(error) {
+        // Handle error
+          res.error(error);
+      }
+    },{ useMasterKey: true });
+      
+});
