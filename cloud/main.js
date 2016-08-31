dd69517dca5a9dabd2b84d0b50b2d2d075ef6767
essamghonim <ee14eno@my.brunel.ac.uk>
2016-08-31 21:53:17 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,27 +3,21 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi Complete change Essam');
 });
 Parse.Cloud.define("iosPushTest", function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
-  var params = request.params;
-  var user = request.user;
-
-  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
-  var messageText = params.text;
-
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
-
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
-    data: {
-      alert: "Message: " + messageText
-    }
-  }, { success: function() {
-      console.log("#### PUSH OK");
-  }, error: function(error) {
-      console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-response.success('success');
+Parse.initialize("essamghonimaucbrunel03121993", "myMasterKeyqazwsxedcrfvtgbyhnujm!@#$%^&*");
+var pushQuery = new Parse.Query(Parse.Installation);
+Parse.Push.send({
+data: {
+alert: 'The Giants Mets 2-3.',
+badge: 1,
+sound: 'default'
+}
+}, {
+success: function() {
+console.log('##### PUSH OK');
+},
+error: function(error) {
+console.log('##### PUSH ERROR');
+},
+useMasterKey: true
+});
 });
