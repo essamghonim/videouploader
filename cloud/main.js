a8ddfe6434e0fef2f361a161dffc6c3a7b91e450
essamghonim <ee14eno@my.brunel.ac.uk>
2016-09-01 03:29:54 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,22 +2,21 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi Complete change Essam');
 });
-Parse.Cloud.define("iosPushTest", function(request, response) {
-var pushQuery = new Parse.Query(Parse.Installation);
-pushQuery.equalTo('deviceType', 'ios');
-Parse.Push.send({
-data: {
-alert: 'The Giants Mets 2-3.',
-badge: 1,
-sound: 'default'
-}
-}, {
-success: function() {
-console.log('##### PUSH OK');
-},
-error: function(error) {
-console.log('##### PUSH ERROR');
-},
-useMasterKey: true
-});
+Parse.Cloud.afterSave("EA2iWMF1H3D123PntxOaGI0jS", function(request) {
+  var messageText = request.object.get('text');
+  var pushQuery = new Parse.Query(Parse.Installation);
+      pushQuery.equalTo('deviceType', 'ios');
+        Parse.Push.send({
+            where: pushQuery, // Set our Installation query
+            data: {
+              alert: "New message: Hello Essam"
+             }
+            }, {
+      success: function() {
+      // Push was successful
+          },
+      error: function(error) {
+        throw "Got an error " + error.code + " : " + error.message;
+          }
+            });
 });
