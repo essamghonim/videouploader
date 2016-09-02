a66bf88796f3c3ee8ce513cd44abe17dc6637ed9
essamghonim <ee14eno@my.brunel.ac.uk>
2016-09-02 19:35:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (process.env.GCM_SENDER_ID && process.env.GCM_API_KEY) {
 if (process.env.APNS_ENABLE) {
     pushConfig['ios'] = [
         {
-            pfx: 'HerokuCert.p12', // P12 file only
+            pfx: __dirname + 'HerokuCert.p12', // P12 file only
             bundleId: 'Com.ConnectLtd.Pal',  // change to match bundleId
             production: false // dev certificate
         }
@@ -46,7 +46,7 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || 'myMasterKeyqazwsxedcrfvtgbyhnujm!@#$%^&*', //Add your master key here. Keep it secret!
   push: pushConfig,
   filesAdapter: filesAdapter,
-  serverURL: process.env.SERVER_URL || 'http://pally.herokuapp.com/parse'  // needed for Parse Cloud and push notifications
+  serverURL: process.env.SERVER_URL || 'https://palscoob.herokuapp.com/parse'  // needed for Parse Cloud and push notifications
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
