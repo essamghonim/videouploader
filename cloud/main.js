b0fe47d3f2df194eda172cd5f62d5ac03a9d74c7
essamghonim <ee14eno@my.brunel.ac.uk>
2016-09-03 00:40:10 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -73,13 +73,13 @@ Parse.Cloud.define("iosPushTest", function(request, response) {
   // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
   var params = request.params;
   //var user = request.user;
-//var user = new Parse.User();
-//user.id = 'RO9GEfkgI6';    
+var user = new Parse.User();
+user.id = 'RO9GEfkgI6';    
   // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
   var messageText = params.text;
 
   var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios');                                                                                                                                        
+  pushQuery.equalTo('user', user);                                                                                                                                        
 
   Parse.Push.send({
     where: pushQuery, // Set our Installation query                                                                                                                                                              
